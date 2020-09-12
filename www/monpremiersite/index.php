716ce3ace0fa3540c63234d31c9e14ae50bf5261
frejus-ken <70434065+frejus-ken@users.noreply.github.com>
2020-09-12 11:39:34 +0200

    Add files via upload

---
+++ www/monpremiersite/index.php
@@ -0,0 +1,48 @@
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+        <h4 class="mb-3">Payment</h4>
+ </div>
+        <div class="row">
+            <div class="col-md-6 mb-3">
+                <label for="cc-number">Card Number</label> <br>
+                <input type="text" class="form-control" id="cc-number" placeholder="" required>
+                <div class="invalid-feedback">
+                </div>
+              
+          <div class="col-md-8 mb-4">
+            <label for="cc-name">Card Name </label> <br>
+            <input type="text" class="form-control" id="cc-name" placeholder="" required>
+            <div class="invalid-feedback">
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+              <tr><label for="cc-expiration">Expiration Date</label><br>
+              <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
+              <div class="invalid-feedback"> </tr>
+            </div>
+            <div class="col-3">
+                <tr><label for="cc-cvv">Years</label> <br>
+                <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
+                <div class="invalid-feedback"> </tr>
+                  </div>
+            <div class="col-3">
+              <label for="cc-cvv">CVV</label> <br>
+              <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
+              <div class="invalid-feedback">
+                </div>
+              </div>
+            </div>
+          </div>
+        <br>
+        <button type="button" class="btn btn-primary btn-lg btn-block">SUBMIT</button>      </form>
+    </div>
+  </div>
+</body>
+</html>
